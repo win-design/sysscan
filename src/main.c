7bde2bc16afa6832dd86bf22634c01795b15aa04
Anass GM <smartt24@gmail.com>
2017-06-11 05:17:56 +0000

    int 1
    no one

--- src/main.c
+++ src/main.c
@@ -1261,7 +1261,7 @@ main_scan(struct sysscan *sysscan)
         now = time(0);
         gmtime_s(&x, &now);
         strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S GMT", &x);
-        LOG(0, "\nStarting sysscan " sysscan_VERSION " (http://bit.ly/14GZzcT) at %s\n", buffer);
+        LOG(0, "\nStarting sysscan version 1.0.1 -> r00t " sysscan_VERSION " (http://goo.gl/1y9cLW) at %s\n", buffer);
         LOG(0, " -- forced options: -sS -Pn -n --randomize-hosts -v --send-eth\n");
         LOG(0, "Initiating SYN Stealth Scan\n");
         LOG(0, "Scanning %u hosts [%u port%s/host]\n",
